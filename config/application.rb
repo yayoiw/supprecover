c328de1a5627701d0bfb66db6c3ce5a2549329ae
yayoiwatabe <lemo.pep@gmail.com>
2023-09-25 20:25:19 +0900

    full medical checkup 基礎データ

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,18 @@ module Supprecover
       neutral_fat: { min: 50, middle: 100, max: 150 },
       ast: { min: 10, middle: 20, max: 30 },
       alt: { min: 10, middle: 20, max: 30 },
-      gamma_gtp: { min: 25, middle: 50, max: 75 }
+      gamma_gtp: { min: 25, middle: 50, max: 75 },
+      fasting_blood_sugar: { min: 70, middle: 85, max: 100 },
+      hba1c: { min: 4.5, middle: 5.0, max: 5.5 },
+      urine_sugar: { min: 0, middle: 1, max: 2 },
+      uric_acid: { min: 2.0, middle: 4.5, max: 7.0 },
+      creatinine: { min: 0.5, middle: 0.6, max: 0.7 },
+      egfr: { min: 60, middle: 75, max:90  },
+      hematocrit: { min: 36, middle: 40, max: 44 },
+      hemoglobin: { min: 13, middle: 14, max: 16 },
+      rbc: { min: 360, middle: 425, max: 490 },
+      wbc: { min: 3100, middle: 5700, max: 8400 },
+      urine_protein: { min: 0, middle: 0, max: 1 }
     }
   end
 end
