74e9825ba3f31ed813fff8f085054db752193c8f
yayoiwatabe <lemo.pep@gmail.com>
2023-11-16 00:34:15 +0900

    強制モーダル修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :redirect_unless_name, except: [:before_use, :create, :about, :terms_of_use, :privacy_policy]
   before_action :blocking_access_before_use, only: :before_use
-  
+  before_action :set_visited, only: :before_use
+
+
   def index
     @user = User.find_by(name: session[:name])
   end
@@ -29,6 +31,13 @@ class UsersController < ApplicationController
   def privacy_policy;end
 
   private
+
+  def set_visited
+    unless cookies[:visited]
+      cookies[:visited] = { value: true, expires: 7.days.from_now }
+      @first_visit = true
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name)
