2582744e6152a5f820fb4eac9dabfc52fe20c5f5
yayoiwatabe <lemo.pep@gmail.com>
2024-01-09 03:24:57 +0900

    easy_medical_checkupのnew,showビュー　マークアップ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,11 @@ class ApplicationController < ActionController::Base
 
   def redirect_unless_name
     return if session[:name].present?
-
     redirect_to before_use_path, warning: t('.warning.no_name')
   end
 
   def blocking_access_before_use
     return unless session[:name].present?
-
     redirect_to root_path, warning: t('.warning.name_exist')
   end
 
